2d482b71477e06b99d50938f20f7add5c48c33bb
LomAndry <lomandri21@gmail.com>
2021-01-17 19:27:56 +0400

    Added files

---
+++ src/Search.java
@@ -0,0 +1,34 @@
+public class Search {
+	static int count;
+	static int a[];
+	static int left; 
+	static int right;
+		
+	static void newArray() {
+		int[] array = new int[12];
+			for (int i = 0; i < array.length; i++) {
+				array[i] = (int) Math.round((Math.random() * 15) - 5);	
+				System.out.println(array[i]);
+		}
+	}
+	int search(int count, int a[], int left, int right){
+		if (left >= right) {
+			return a[left] == count? left:-1;
+		}
+		int mid = (left + right) / 2;
+		if (a[mid] == count) {
+			return mid;
+		}
+		else if (a[mid] < count){
+			return search(count, a, left, mid-1);
+		}
+		else{
+			return search(count, a, mid+1, right);
+		}
+	}
+		public static void main(String[] args) {
+			newArray();			
+		}		
+	}
+
+	
